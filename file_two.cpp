4ff601d669a78c185ec221e34bb46f45cb5637ef
HalaaHaytham <halaahaitham2@gmail.com>
2025-08-01 04:56:10 +0300

    Added code to file two

---
+++ file_two.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your sentence" << endl;
+    getline(cin, name);
+    reverse(name.begin(), name.end());
+    cout << "Reversed sentence " << name << endl;
+}
